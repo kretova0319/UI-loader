5dfe4e40492b5c236377aaf1c0f5b49ad816e2ed
kretova0319 <kretova0319@gmail.com>
2024-11-17 23:11:57 +0400

    init: new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import LoaderPage from "./LoaderPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [stateloader, setstateLoader] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setstateLoader(false), 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return <div>{stateloader && <LoaderPage />}</div>;
 }
 
 export default App;
---
+++ src/LoaderPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Loader from "./Loader";
+
+export default function LoaderPage() {
+  return (
+    <div className="container_loader ">
+      <Loader />
+    </div>
+  );
+}
